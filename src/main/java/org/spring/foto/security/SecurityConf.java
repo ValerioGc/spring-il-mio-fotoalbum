e246c931456359255975bb35edb1826b89d04960
ValerioGc <valerio.gc1@gmail.com>
2022-12-20 12:25:43 +0100

    revisione security

--- src/main/java/org/spring/foto/security/SecurityConf.java
+++ src/main/java/org/spring/foto/security/SecurityConf.java
@@ -3,7 +3,6 @@ package org.spring.foto.security;
 import org.spring.foto.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
