72bf14ed9103f792be7d94abf6d614f2fb4fb510
ValerioGc <valerio.gc1@gmail.com>
2022-12-20 10:41:53 +0100

    creazione service pojo category

--- src/main/java/org/spring/foto/service/CategoryService.java
+++ src/main/java/org/spring/foto/service/CategoryService.java
@@ -1,8 +1,51 @@
 package org.spring.foto.service;
 
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.spring.foto.pojo.Category;
+import org.spring.foto.repo.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CategoryService {
 
+	@Autowired
+	private CategoryRepo categoryRepo;
+	
+// Save
+	public void save(Category categories) {
+		categoryRepo.save(categories);
+	}
+		
+// Find
+	public List<Category> findAll() {
+		return categoryRepo.findAll();
+	}
+	
+// Find by ID
+	public Category findCategoryById(int id) {
+		return categoryRepo.findById(id).get();
+	}
+	
+// Delete by ID
+	public void deleteCategoryById(int id) {
+		categoryRepo.deleteById(id);
+	}
+	
+// Find pizzas
+	@Transactional
+	public List<Category> findAllCategories() {
+		
+		List<Category> categories = categoryRepo.findAll();
+		
+		for (Category cat : categories) {
+			Hibernate.initialize(cat.getPhotos());
+		}
+		
+		return categories;
+	}
 }
