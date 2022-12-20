8e65cb691da9bc391878fa7e0558c9f345399cc7
ValerioGc <valerio.gc1@gmail.com>
2022-12-20 16:38:58 +0100

    Implementazione show

--- src/main/java/org/spring/foto/controller/AdminPhotoAreaController.java
+++ src/main/java/org/spring/foto/controller/AdminPhotoAreaController.java
@@ -1,6 +1,7 @@
 package org.spring.foto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.spring.foto.pojo.Photo;
 import org.spring.foto.service.PhotoService;
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin/photo")
@@ -31,7 +35,44 @@ public class AdminPhotoAreaController {
 	}
 	
 	
-//  Cancella foto ----------------------------------------------------------------
+//  Show foto ----------------------------------------------------------------
+	@GetMapping("/show/{id}")
+	public String showPhotos(@PathVariable("id") int id, Model model) {
+		
+		Optional<Photo> optPhoto = photoService.findPhotoById(id);
+		Photo photo = optPhoto.get();
+		
+		model.addAttribute("photo", photo);
+		
+		model.addAttribute("routeName", "dettaglio foto");
+		 
+		return "admin-show";
+	}
+	
+	
+//  Crea foto ----------------------------------------------------------------
+	@GetMapping("/create/{id}")
+	public String createPizza(@PathVariable("id") int id) {
+		return "";
+	}
+	@PostMapping("/store")
+	public String storePhoto(@Valid Photo photo) {
+		return "";
+	}
+	
+	
+//  Modifica foto ------------------------------------------------------------
+	@GetMapping("/edit/{id}")
+	public String editPizza(@PathVariable("id") int id) {
+		return "";
+	}
+	@PostMapping("/update")
+	public String updatePhoto(@Valid Photo photo) {
+		return "";
+	}
+	
+	
+//  Cancella foto ------------------------------------------------------------
 	@GetMapping("/delete/{id}")
 	public String deletePizza(@PathVariable("id") int id) {
 		
