fc4dac2b9f30bd6939d39c33346637c70e0db6be
ValerioGc <valerio.gc1@gmail.com>
2022-12-21 12:41:02 +0100

    Creazione controllerapi

---
+++ src/main/java/org/spring/foto/api/ApiController.java
@@ -0,0 +1,25 @@
+package org.spring.foto.api;
+
+import java.util.List;
+
+import org.spring.foto.pojo.Photo;
+import org.spring.foto.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1")
+@CrossOrigin("*")
+public class ApiController {
+	
+	@Autowired
+	private PhotoService photoService;
+	
+	@GetMapping("/photo/index")
+	public List<Photo> getPhotos() {
+		return photoService.findAll();
+	}
+}
